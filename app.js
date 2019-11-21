1ff01181e1db3d1bc8a92a5c86382ddda687abc8
Anthony Cera <ace_cera@yahoo.com>
2019-11-20 17:32:52 -0800

    added the logic to read the input data typein the UI controller to then pass it to the appController.

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
+//Budget Controller 
 var budgetController = (function() {
-    var x = 23;
 
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return (add(b));
-        }
-    }
 })();
 
+//UI Controller 
 var UIController = (function() {
-
-})();
-
-var appController = (function(budgetCtrl, UICtrl) {
-
-    var z = budgetCtrl.publicTest(5);
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
 
     return {
-        anotherPublic: function() {
-            console.log(z);
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //This will either be inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
         }
+    };
+})();
+
+//Global App Controller
+var appController = (function(budgetCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings();
+
+    var ctrlAddItem = function() {
+         //Get the filled input data
+         var input = UICtrl.getInput();
+         console.log(input);
+
+        //Add input to budget controller
+
+        //Add item to the UI
+
+        //Calculate budget
+
+        //Display budget on the UI
+
+
     }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 
 })(budgetController, UIController);
